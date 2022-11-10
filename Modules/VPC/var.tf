bcca56e0e981688ee510c4de358b637a1b5f1da4
Felipe Rafael Franco Meira <45401843+felipebugo@users.noreply.github.com>
2022-11-10 10:42:35 -0300

    Update var.tf

--- Modules/VPC/var.tf
+++ Modules/VPC/var.tf
@@ -1,82 +1,57 @@
-#ELB
-resource "aws_lb_target_group" "tg_alb_application" {
-  name     = "tg-alb-application"
-  vpc_id   = "${var.vpc_id}"
-  protocol = "${var.protocol}"
-  port     = "${var.port}"
+#Vars Create VPC
 
-  tags = {
-    Name = "tg-alb-application"
-  }
+variable "vpc_dev_cidr" {
+  description = "criação da variável o cidr block da VPC"
+  default = "10.0.0.0/16"
 }
 
-resource "aws_lb_listener" "listener_alb_application" {
-  load_balancer_arn = aws_lb.elb_application.arn
-  protocol          = "${var.protocol}"
-  port              = "${var.port}"
-
-  default_action {
-      type             = "${var.ec2_lb_listener_action_type}"
-      target_group_arn = aws_lb_target_group.tg_alb_application.arn
-  }
+variable "vpc_dev_dns_hostname" {
+  description = "declaração da variavel dns hostname"
+  default = true
 }
 
-resource "aws_lb" "elb_application" {
-  name               = "elb-application"
-  load_balancer_type = "application"
-  subnets            = ["${var.sn_vpc_dev_pub_1a_id}", "${var.sn_vpc_dev_pub_1b_id}"]
-  security_groups    = ["${var.vpc_dev_security_group_pub_id}"]
-
-  tags = {
-    Name = "elb-application"
-  }
+variable "vpc_dev_dns_support" {
+  description = "declaração da variavel dns support"
+  default = true
 }
 
 
-#AMI
-data "template_file" "user_data" {
-  template = "${file("./Modules/ASG_ALB/userdata.sh")}"
-  vars = {
-      rds_endpoint = "${var.rds_endpoint}"
-      rds_user     = "${var.rds_user}"
-      rds_password = "${var.rds_password}"
-      rds_name     = "${var.rds_name}"
-  }
+#Vars Subnets Pub
+
+variable "sn_vpc_dev_pub_1a_cidr" {
+  description = "Declaração do ip e cidr block que a subnet pub 1A irá usar"
+  default = "10.0.101.0/24"
 }
 
-resource "aws_launch_template" "template_ASG" {
-  name = "template_ASG"
-  image_id               = "${var.ami}"
-  instance_type          = "${var.instance_type}"
-  vpc_security_group_ids = ["${var.vpc_dev_security_group_pub_id}"]
-  key_name               = "${var.ssh_key}"
-  user_data              = "${base64encode(data.template_file.user_data.rendered)}"
-
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name = "ws-"
-    }
-  }
-
-  tags = {
-        Name = "template_ASG"
-    }
+variable "sn_vpc_dev_pub_1b_cidr" {
+  description = "Declaração do ip e cidr block que a subnet pub 1B irá usar"
+  default = "10.0.102.0/24"
 }
 
-#ASG
-resource "aws_autoscaling_group" "asg" {
-  name                = "asg"
-  vpc_zone_identifier = ["${var.sn_vpc_dev_pub_1a_id}", "${var.sn_vpc_dev_pub_1b_id}"]
-  desired_capacity    = "${var.desired_capacity}"
-  min_size            = "${var.min_size}"
-  max_size            = "${var.max_size}"
-  target_group_arns   = [aws_lb_target_group.tg_alb_application.arn]
-
-  launch_template {
-    id      = aws_launch_template.template_ASG.id
-    version = "$Latest"
-  }
+variable "vpc_sn_pub_map_public_ip_on_launch" {
+    type    = bool
+    default = true
 }
 
 
+#Vars Subnets Priv
+
+variable "sn_vpc_dev_priv_1a_cidr" {
+  description = "Declaração do ip e cidr block que a subnet priv 1A irá usar"
+  default = "10.0.1.0/24"
+}
+
+variable "sn_vpc_dev_priv_1b_cidr" {
+  description = "Declaração do ip e cidr block que a subnet priv 1B irá usar"
+  default = "10.0.2.0/24"
+}
+
+variable "sn_vpc_dev_priv_2a_cidr" {
+  description = "Declaração do ip e cidr block que a subnet priv 1A irá usar"
+  default = "10.0.3.0/24"
+}
+
+variable "sn_vpc_dev_priv_2b_cidr" {
+  description = "Declaração do ip e cidr block que a subnet priv 1B irá usar"
+  default = "10.0.4.0/24"
+}
